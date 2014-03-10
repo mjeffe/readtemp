e7dd8c7183d0c5bf15400a8265ef35db77972acc
u35616872 <u35616872@ee1204b6-b492-43b1-9071-a66a32383116>
2014-03-10 15:14:58 +0000

    Modified to read 2 sensors, one on pin 4 and 1 on pin 17.

--- readtemp.pl
+++ readtemp.pl
@@ -3,28 +3,33 @@
 use strict;
 use warnings;
 
-my $RT = '/home/pi/src/readtemp/readtemp 4';
+my $RT = '/home/pi/src/readtemp/readtemp';
+
+# sensors, and which gpio pin they are connected to
+my @temp_sensor_pins = (4,17);
 
 while (1) {
-   my $data = '';
-   do {
-      $data = `$RT`;
-      chomp($data);
-      if ( $data ) {
-         #print scalar localtime . '|' . join('|', split(/\s/, $data)) . "\n";
-         my ($sec,$min,$hour,$day,$mon,$year,$wday,$yday,$isdst) = localtime;
-         $year += 1900;
-         printf("%d-%02d-%02d %02d:%02d:%02d|%s\n", 
-            $year,$mon,$day,$hour,$min,$sec,join('|', split(/\s/, $data)));
-      } else {
-         #print "sleeping 3\n";
-         sleep(3);
-      }
-   } while ( ! $data );
 
-   $data = '';
-   #print "sleeping 27\n";
-   sleep(27);  # 27 + 3 = 30 second loop
+   for ( my $i=0; $i < scalar @temp_sensor_pins; $i++ ) {
+      my $data = '';
+      do {
+         $data = `$RT $temp_sensor_pins[$i]`;
+         chomp($data);
+         if ( $data ) {
+            #print scalar localtime . '|' . join('|', split(/\s/, $data)) . "\n";
+            my ($sec,$min,$hour,$day,$mon,$year,$wday,$yday,$isdst) = localtime;
+            $year += 1900;
+            printf("%d|%d-%02d-%02d %02d:%02d:%02d|%s\n", 
+               $i,$year,$mon,$day,$hour,$min,$sec,join('|', split(/\s/, $data)));
+         } else {
+            #print "sleeping 3\n";
+            sleep(3);
+         }
+      } while ( ! $data );
+   }
+
+   #print "sleeping 30\n";
+   sleep(30);  
 }
 
 
