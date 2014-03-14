24e6e3719ee63dc768fd592cc230c07ec888c86b
u35616872 <u35616872@ee1204b6-b492-43b1-9071-a66a32383116>
2014-03-14 18:49:35 +0000

    Added some comments that may help understand what is going on.

--- readtemp.c
+++ readtemp.c
@@ -98,7 +98,8 @@ int readDHT(int pin) {
    int laststate = HIGH;
    int j=0;
 
-   /* send DHT11 the start signal */
+   /* send DHT11 the start signal.
+    * This seems to be the opposite sequence from what the datasheet says... */
    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP); // Set GPIO pin to output
    bcm2835_gpio_write(pin, HIGH);
    usleep(500000);  // 500 ms
@@ -110,7 +111,7 @@ int readDHT(int pin) {
    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT); // Set GPIO pin to input
    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
 
-   // wait for pin to drop? not sure what or why...
+   /* wait for pin to drop. This signals begin of sensor's sending data */
    while (bcm2835_gpio_lev(pin) == 1) {
       usleep(1);
    }
@@ -118,17 +119,14 @@ int readDHT(int pin) {
    /*
     * The sensor will be transmitting it's data by changing the value from low
     * to high to low and so on. If the bit to be transmitted is 1, the high
-    * state of the pin will be longer, if it's 0, it will be shorter.
+    * state of the pin will last 70 uSeconds, if it's 0, it will last 26-28 uSec.
     *
-    * So what your code is doing is to probe the value in a loop and watch if
-    * the signal level changes, counting number of loops that the signal did
-    * not change. This way you know for how long it was in the last known state
-    * (counter variable). This counter is then used to check if the bit that
-    * was send is 1 or 0. If the counter value get's to 1000, however, the loop
-    * will brake and data reading will be finished.
-    *
-    * This should happen before MAXTIMINGS bits where transmitted but this
-    * value is a safeguard if something went wrong. 
+    * Here we loop, watching for the pin level to change. Then we count the
+    * number of loops that the signal level did not change. Based on that we
+    * can estimate the length of the signal in uSeconds. If the counter value
+    * get's to 1000, however, the loop ** will brake and data reading will be
+    * finished. This should happen before MAXTIMINGS bits where transmitted 
+    * but this value is a safeguard if something went wrong. 
     */
    // read data!
    for (int i=0; i< MAXTIMINGS; i++) {
@@ -136,6 +134,7 @@ int readDHT(int pin) {
       while ( bcm2835_gpio_lev(pin) == laststate) {
          counter++;
          //nanosleep(1);      // overclocking might change this?
+         /* seems like we should have a usleep(1) here. Arduino code does... */
          if (counter == 1000)
             break;
       }
@@ -143,10 +142,24 @@ int readDHT(int pin) {
       if (counter == 1000) break;
       bits[bitidx++] = counter;
 
+      /*
+       * I don't understand why counter is compared to 200.
+       * The counter is updated ~ every uSec? According to the data sheet,
+       * a logical 1 is when the pin is held in a hight state for 70 uSec
+       * so it seems like we should be comparing it to 70 uSec. Perhaps
+       * the 200 is accounting for the CPU going off and doing other things?
+       *
+       * note: j/8 points to the same data[] bucket for 8 consecutive 
+       * values of j. That is 1 byte's worth of data.
+       *
+       * Also, we ignore the first three transitions. Those are the LOW,
+       * HIGH, LOW signalling the beggining of sensor's data transmision.
+       * I think the i%2 says, we only care about every other transition,
+       * that is, we only want to push the data bit on the HIGH to LOW
+       * transition, not the LOW to HIGH.
+       */
       if ((i>3) && (i%2 == 0)) {
          // shove each bit into the storage bytes
-         // note: j/8 points to the same data[] bucket for 8 consecutive 
-         // values of j. That is 1 byte's worth of data.
          data[j/8] <<= 1;
          if (counter > 200)
             data[j/8] |= 1;
@@ -165,6 +178,12 @@ int readDHT(int pin) {
    if ( verbose )
       printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%x\n", j, data[0], data[1], data[2], data[3], data[4]);
 
+   /*
+    * Arduino code has the following comment here. Also has (j >= 40) :
+    *
+    * // check we read 40 bits and that the checksum matches
+    *
+    */
    if ((j >= 39) && (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
       // yay!
       if ( verbose )
